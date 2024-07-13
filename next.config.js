52613b1c2f64ba15157e3b30b406e5e160da1918
Hariom Sharma <hariom.s2510@gmail.com>
2024-07-14 01:21:51 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '**'
-      }
-    ]
-  }
-}
+module.exports = {}
